429ab7ea7286429e88cb3711b23d33c7a40b0e96
EtnoPolino <einshoch66@gmail.com>
2022-06-13 21:35:49 +0200

    finaly done

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # etch-a-sketch
+
+Project from Odin Project to do.
+
+## Table of contents
+- [Overview](#overview)
+  - [Links](#links)
+  - [Built with](#built-with)
+
+
+## Overview
+Project from Odin Project to do. Didnt do all the optional, only random color was done.
+
+
+### Links
+
+- Solution URL : [code](https://github.com/EtnoPolino/etch-a-sketch.git)
+- Live Site URL : [demo](https://etnopolino.github.io/etch-a-sketch/)
+
+
+### Built with
+
+- HTML 
+- CSS
+- Javascript vanilla
--- script.js
+++ script.js
@@ -1,40 +1,52 @@
+let divisions;
+let isBlackActive = false;
+let isRandomActive = false;
 const inputGrid = document.querySelector('input');
 const wrapper = document.querySelector('.wrapper');
 const submit = document.querySelector('.submit');
+const blackColor = document.querySelector('.black-color');
+const randomColor = document.querySelector('.random-color');
 const clearButton = document.querySelector('.clear-grid');
-/*const choiceButtons = document.querySelectorAll('button');*/
 
-let isBlackActive = false;
-let isRandomActive = false;
+
+
+createSketch(inputGrid.value);   /* initialisation */
+colorDivision(divisions);    
+
 /*-------------------------------------------------------------------------------------------------------------
-                                    AddeventListeners
+                                    Add Event Listener
 -------------------------------------------------------------------------------------------------------------*/
-submit.addEventListener('click', createSketch)
-clearButton.addEventListener('click', clearBoard);
 
-/*choiceButtons.forEach((button) => {
-    button.addEventListener('click', test);
-});*/
+blackColor.addEventListener('click', chooseBlack);
+randomColor.addEventListener('click', chooseRandom);
+
+clearButton.addEventListener('click', ()=>{
+    clearGrid();
+    createSketch(inputGrid.value);
+    colorDivision(divisions);
+});
+
+submit.addEventListener('click', ()=>{
+    clearGrid();
+    createSketch(inputGrid.value);
+    colorDivision(divisions); 
+})
 
 
 /*-------------------------------------------------------------------------------------------------------------
                                     functions
 -------------------------------------------------------------------------------------------------------------*/
-function createSketch(){
-
-const blackColor = document.querySelector('.black-color');
+function createSketch(size){
     let numberOfDivs;
 
-    removeElement();
-
-    if (inputGrid.value > 100) {
+    if(size > 100){
         numberOfDivs = 100*100;
-    }else if(inputGrid.value < 0){
-        inputGrid.value = '';
-        numberOfDivs = 0;
+    }else if(size <= 0){
+        numberOfDivs = 1
     }else{
-        numberOfDivs = inputGrid.value*inputGrid.value;
+        numberOfDivs = size*size;
     }
+
     
     for(let i = 1; i <= numberOfDivs; i++){
         const div = document.createElement('div');
@@ -42,162 +54,51 @@ const blackColor = document.querySelector('.black-color');
         wrapper.appendChild(div);
     }
 
-    if(inputGrid.value > 100){
+    if(size > 100){
         wrapper.style.gridTemplateColumns = `repeat(100,1fr)`;
         wrapper.style.gridTemplateRows = `repeat(100,1fr)`;
+    }else if(size <= 0){
+        wrapper.style.gridTemplateColumns = `repeat(1,1fr)`;
+        wrapper.style.gridTemplateRows = `repeat(1,1fr)`;
     }else{
-        wrapper.style.gridTemplateColumns = `repeat(${inputGrid.value},1fr)`;
-        wrapper.style.gridTemplateRows = `repeat(${inputGrid.value},1fr)`;
+        wrapper.style.gridTemplateColumns = `repeat(${size},1fr)`;
+        wrapper.style.gridTemplateRows = `repeat(${size},1fr)`;
     }
-
-/* it needs to be there because our created div now exist, before they just dont. */    
-const boxes = document.querySelectorAll('.box');
-const black = document.querySelector('.black-color');
-const random = document.querySelector('.random-color'); 
     
-    black.addEventListener('click', ()=>{
-        isBlackActive = true;
-        black.classList.toggle('active');
+    divisions = document.querySelectorAll('.box');   
+}
 
-        isRandomActive = false;
-        random.classList.remove('active');
+function clearGrid(){
+    divisions.forEach((division) => wrapper.removeChild(division));
+}
 
-    });
+function chooseBlack(){
+    isBlackActive = !isBlackActive;
+    this.classList.toggle('active');
 
-    random.addEventListener('click', ()=>{
-        isRandomActive = true;
-        random.classList.toggle('active');
+    isRandomActive = false;
+    randomColor.classList.remove('active')
+}
 
-        isBlackActive = false;
-        black.classList.remove('active');
+function chooseRandom(){
+    isRandomActive  = !isRandomActive;
+    this.classList.toggle('active');
+
+    isBlackActive = false;
+    blackColor.classList.remove('active');
+}
+
+function colorDivision(arguments){   
+    arguments.forEach((argument)=>{
+        argument.addEventListener('mouseover', ()=>{
+            if(isBlackActive){
+                argument.style.backgroundColor = 'black';
+            }else if(isRandomActive){
+                let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                argument.style.backgroundColor = '#' + randomColor;
+            }else{
+                argument.style.backgroundColor = 'lightblue';
+            }
+        })
     })
-
-    console.log('black active ? '+isBlackActive);
-    console.log('random active ? '+isRandomActive);
-    
-    boxes.forEach( box => {
-        box.addEventListener('mouseover', ()=>{
-            console.log('test')
-        });
-    });
-
-    
-
-
-} // end of function createSketch
-
-function clearBoard(){
-    inputGrid.value = '';
-    removeElement();
 }
-
-function removeElement(){
-    while(wrapper.lastElementChild){
-        wrapper.removeChild(wrapper.lastElementChild);
-    }
-}
-
-function colorBox(){
-    this.style.backgroundColor = 'black';
-}
-
-function colorRandom(){
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    this.style.backgroundColor = '#' + randomColor;
-}
-
-function test(){
-    choiceButtons.forEach((button) => {
-        button.classList.remove('active');
-    });
-
-    this.classList.add('active');
-}
-
-function draw(){
-    if(isBlackActive){
-        this.style.backgroundColor = 'black';
-    } else if (isRandomActive){
-        let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        this.style.backgroundColor = '#' + randomColor;
-    }
-}
-
-
-/*
-const boxes = document.querySelectorAll('.box');
-const randomColor = document.querySelector('.random-color');
-const buttonChangeColors = document.querySelectorAll('.buttons');
-
-    boxes.forEach( box => {
-        box.addEventListener('mouseover', colorBox);
-    }); */
-/*
-buttonChangeColors.forEach((button) => {
-    button.addEventListener('click', test);
-});*/
-
-
-/*
-j'appuis sur le bouton 'random color', il y'a une couleur noir qui se met...
-    - on a des couleur random
-quand je rappuis, j'enlève la couleur random, 
-    - on a une couleur noir.
- 
-
-
-*/
-
-
-
-
-/* 
-TO DO
-
-- mettre un toggle au bouton RandomCOlor pour mettre et enlever les couleur;
-- le boutton clear doit aussi enlever les couleurs de randomColor.
-- enlever le fait de mettre 0 et moins
-
-*/
-
-/*    
-    randomColor.addEventListener('click', () => {
-        randomColor.classList.toggle('active');
-
-        if(randomColor.classList.contains('active')){
-            boxes.forEach( box => {
-                box.addEventListener('mouseover', colorRandom);
-            });
-        } else {
-            boxes.forEach( box => {
-                box.addEventListener('mouseover', colorBox);
-            });
-        }
-
-        if(!randomColor.classList.contains('active') ){
-            boxes.forEach( box => {
-                box.addEventListener('mouseover', ()=>{
-                    box.style.backgroundColor = 'black';
-                });
-            });
-        }
-        
-    }) */
-
-
-/* donc pour que ça marche
-
-
-quand j'appuis sur un boutton 1...
-    si ce bouton à la classe active ET black-color
-        alors le mouseover de box est noir
-       
-quand j'appuis sur un boutton 2...
-    si ce bouton à la classe active ET random-color
-        alors le mouseover de box est random.
-
-fin.
-
-
-
-    */
